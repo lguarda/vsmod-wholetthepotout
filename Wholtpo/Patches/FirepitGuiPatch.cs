bbec4a149ac1ca8f2a2eeabe9ad884c4aa42d81b
lguarda <leoman037@live.fr>
2026-05-09 11:43:15 +0200

    clean: now it work code is clean i can probably release

--- Wholtpo/Patches/FirepitGuiPatch.cs
+++ Wholtpo/Patches/FirepitGuiPatch.cs
@@ -3,64 +3,7 @@ using Vintagestory.GameContent;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
-
-using Vintagestory.API.Client;
-using Vintagestory.API.Common;
-using Vintagestory.API.Config;
-using Vintagestory.API.Common.Entities;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using Vintagestory.API.MathTools;
-using Vintagestory.GameContent;
-using HarmonyLib;
-
-//public static class FirepitPatch {
-//
-//    private static readonly FieldInfo _inventoryField =
-//        typeof(BlockEntityFirepit).GetField("inventory", BindingFlags.NonPublic | BindingFlags.Instance);
-//
-//    private static bool detectMealinFirepit(IInventory inv) {
-//        var potStack = inv[2].Itemstack;
-//        return potStack != null && potStack.Collectible.GetType().Name == "BlockCookingContainer";
-//    }
-//
-//    private static void fixFirePitCookingPot(ICoreClientAPI capi, BlockEntityFirepit firepit) {
-//        var inv = _inventoryField?.GetValue(firepit) as IInventory;
-//        if (inv == null)
-//            return;
-//        if (inv[1].Empty && detectMealinFirepit(inv)) {
-//            var packet = inv.TryFlipItems(1, inv[2]);
-//            if (packet != null)
-//                capi.Network.SendPacketClient(packet);
-//        }
-//    }
-//
-//    [HarmonyPatch(typeof(BlockEntityFirepit), "OnSlotModified")]
-//    public static void SlotModifiedPostfix(BlockEntityFirepit __instance) {
-//        var capi = __instance.Api as ICoreClientAPI;
-//        if (capi == null) {
-//            return;
-//        }
-//
-//        capi.Logger.Error("OMG");
-//        capi.Event.EnqueueMainThreadTask(() => { fixFirePitCookingPot(capi, __instance); }, "Wholtpo");
-//    }
-//
-//    [HarmonyPatch(typeof(BlockEntityOpenableContainer), "toggleInventoryDialogClient")]
-//    public static void Postfix(BlockEntityOpenableContainer __instance) {
-//        if (__instance is not BlockEntityFirepit firepit)
-//            return;
-//        var capi = __instance.Api as ICoreClientAPI;
-//
-//        // This may be on server side?
-//        if (capi == null)
-//            return;
-//
-//        capi.Logger.Error("OMG");
-//        capi.Event.EnqueueMainThreadTask(() => { fixFirePitCookingPot(capi, firepit); }, "Wholtpo");
-//    }
-//}
 
 public static class FirepitPatchHelper {
     private static readonly FieldInfo _inventoryField =
@@ -73,7 +16,8 @@ public static class FirepitPatchHelper {
 
     public static void FixFirePitCookingPot(ICoreClientAPI capi, BlockEntityFirepit firepit) {
         var inv = _inventoryField?.GetValue(firepit) as IInventory;
-        if (inv == null) return;
+        if (inv == null)
+            return;
         if (inv[1].Empty && DetectMealInFirepit(inv)) {
             var packet = inv.TryFlipItems(1, inv[2]);
             if (packet != null)
@@ -91,7 +35,8 @@ public static class FirepitSlotPatch {
             return;
         }
 
-        capi.Event.EnqueueMainThreadTask(() => { FirepitPatchHelper.FixFirePitCookingPot(capi, __instance); }, "Wholtpo");
+        capi.Event.EnqueueMainThreadTask(() => { FirepitPatchHelper.FixFirePitCookingPot(capi, __instance); },
+                                         "Wholtpo");
     }
 }
 
--- Wholtpo/WholtpoModSystem.cs
+++ Wholtpo/WholtpoModSystem.cs
@@ -1,12 +1,5 @@
 ﻿using Vintagestory.API.Client;
 using Vintagestory.API.Common;
-using Vintagestory.API.Config;
-using Vintagestory.API.Common.Entities;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using Vintagestory.API.MathTools;
-using Vintagestory.GameContent;
 using HarmonyLib;
 
 namespace Wholtpo {
@@ -15,33 +8,10 @@ public class WholtpoModSystem : ModSystem {
     public override void StartClientSide(ICoreClientAPI api) {
         Mod.Logger.Notification("Wholtpo starting");
         var harmony = new Harmony("glideview");
-        //harmony.PatchAll();
-        harmony.PatchAll(Assembly.GetExecutingAssembly());
-        foreach (var method in Harmony.GetAllPatchedMethods())
-            api.Logger.Debug($"[wholtpo] Patched: {method.Name}");
-        //{
-        //  var method =
-        //      AccessTools.Method(typeof(BlockEntityFirepit), "OnSlotModified");
-        //  if (method == null) {
-        //    api.Logger.Error(
-        //        $"Wholtpo: fail to found method OnSlotModified  stopping the mod");
-        //    return;
-        //  }
-        //  harmony.Patch(method, postfix: new HarmonyMethod(typeof(FirepitPatch),
-        //                                                   "SlotModifiedPostfix"));
-        //}
-
-        //{
-        //  var method = AccessTools.Method(typeof(BlockEntityOpenableContainer),
-        //                                  "toggleInventoryDialogClient");
-        //  if (method == null) {
-        //    api.Logger.Error(
-        //        $"Wholtpo: fail to found method toggleInventoryDialogClient stopping the mod");
-        //    return;
-        //  }
-        //  harmony.Patch(
-        //      method, postfix: new HarmonyMethod(typeof(FirepitPatch), "Postfix"));
-        //}
+        harmony.PatchAll();
+        // harmony.PatchAll(Assembly.GetExecutingAssembly());
+        // foreach (var method in Harmony.GetAllPatchedMethods())
+        //     api.Logger.Debug($"wholtpo Patched: {method.Name}");
     }
 }
 }
